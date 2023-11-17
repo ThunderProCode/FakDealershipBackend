6422e559cd88b3d70ffc7d999698d62c0cbb8a84
thunderprocode <hectora887ju@gmail.com>
2023-11-17 13:31:06 -0600

    Car controller updated to use IActionResult and Update method added

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -2,6 +2,7 @@ using FakeDealerAPI.Auth;
 using FakeDealerAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FakeDealerAPI.Controllers;
 
@@ -17,28 +18,75 @@ public class CarController: ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Car> GetAll(){
-        return _context.Cars;
+    public async Task<IActionResult> GetAll([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+    {
+        var cars = await _context.Cars
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+        return Ok(cars);
     }
 
-    [Authorize]
     [HttpPost]
-    public void Create(Car car)
+    [Authorize]
+    public async Task<IActionResult> Create([FromBody] Car car)
     {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
         _context.Cars.Add(car);
-        _context.SaveChanges();
+        await _context.SaveChangesAsync();
+        return Ok();
     }
 
+    [HttpDelete("{id}")]
     [Authorize]
-    [HttpDelete]
-    public void Delete(int id)
+    public async Task<IActionResult> Delete(int id)
     {
-        var car = _context.Cars.Find(id);
-        if(car != null)
+        var car = await _context.Cars.FindAsync(id);
+        if (car == null)
         {
-            _context.Cars.Remove(car);
-            _context.SaveChanges();
+            return NotFound();
         }
+
+        _context.Cars.Remove(car);
+        await _context.SaveChangesAsync();
+        return NoContent();
     }
+
+    [HttpPut("{id}")]
+    [Authorize]
+    public async Task<IActionResult> Update(int id, [FromBody] Car updatedCar)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var existingCar = await _context.Cars.FindAsync(id);
+
+        if (existingCar == null)
+        {
+            return NotFound();
+        }
+
+        existingCar.Make = updatedCar.Make ?? existingCar.Make;
+        existingCar.Model = updatedCar.Model ?? existingCar.Model;
+        existingCar.Year = updatedCar.Year ?? existingCar.Year;
+        existingCar.Mileage = updatedCar.Mileage ?? existingCar.Mileage;
+        existingCar.Mpg = updatedCar.Mpg ?? existingCar.Mpg;
+        existingCar.Description = updatedCar.Description ?? existingCar.Description;
+        existingCar.Price = updatedCar.Price;
+        existingCar.VIN = updatedCar.VIN ?? existingCar.VIN;
+        existingCar.Images = updatedCar.Images ?? existingCar.Images;
+        // Update other properties as needed...
+
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+    
 
 }
